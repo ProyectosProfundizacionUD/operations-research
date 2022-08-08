5822f56838a9125226f8d8bd7896244b503b941b
Diego Restrepo <alejorvela@gmail.com>
2022-08-07 23:07:52 -0500

    feat: added first matrix generation

--- frontend/src/app/methods/simplex-two-fases/simplex-two-fases.component.ts
+++ frontend/src/app/methods/simplex-two-fases/simplex-two-fases.component.ts
@@ -51,6 +51,8 @@ export class SimplexTwoFasesComponent implements OnInit {
   }
 
   startSimplex() {
+    console.log("Operacion y restricciones"); // !to delete
+    // !to delete
     console.log(this.equations); // !to delete
     console.log(this.operationEquation); // !to delete
     
@@ -72,26 +74,94 @@ export class SimplexTwoFasesComponent implements OnInit {
   }
 
   simplexProcess(validationsType: any, rValue: number){
-    let countCXCJwFields = 0;
-    let countXbFields = 0;
+    this.initialMatrix = this.equations;
+
+    let countNewVariables = 0;
+    let countTotalRows = 0;
+    let CxCjText = "{"
+    let ZjCjText = "{"
+    let CXCJ = {};
+    let ZjCj = [];
+    let R = 1;
+    let H = 1;
+    let S = 1;    
+    for (let o = 0; o < this.variablesCount; o++) {
+      ZjCjText += `"X${o+1}": 0,`
+      CxCjText += `"X${o+1}": 0,`
+    }
     for (let i = 0; i < this.restrictionsCount; i++) {
       switch (this.equations[i].op) {
         case "≥":
-          countCXCJwFields += 2;
-          countXbFields += 1;
+          for (let k = 0; k < this.restrictionsCount; k++) {
+            this.initialMatrix[k][`S${S}`] = 0;
+            this.initialMatrix[k][`R${R}`] = 0;
+          }
+          this.initialMatrix[i][`S${S}`] = -1;
+          this.initialMatrix[i][`R${R}`] = 1;
+          this.initialMatrix[i][`CxCj${i}`] = rValue;
+
+          countNewVariables += 2;
+          countTotalRows += 1;
+          ZjCjText += `"S${S}": 0,`;
+          ZjCjText += `"R${R}": 0,`;
+
+          CxCjText += `"S${S}": 0,`;
+          CxCjText += `"R${R}": ${rValue},`;
+          S += 1;
+          R += 1;
           break;
         case "≤":
-          countCXCJwFields += 1;
-          countXbFields += 1;
+          for (let k = 0; k < this.restrictionsCount; k++) {
+            this.initialMatrix[k][`H${H}`] = 0;
+          }
+          this.initialMatrix[i][`H${H}`] = 1;
+          this.initialMatrix[i][`CxCj${i}`] = 0;
+
+          countNewVariables += 1;
+          countTotalRows += 1;
+          ZjCjText += `"H${H}": 0,`;
+          CxCjText += `"H${H}": 0,`;
+          H += 1;
           break;
         case "=":
-          countCXCJwFields += 1;
-          countXbFields += 1;
+          for (let k = 0; k < this.restrictionsCount; k++) {
+            this.initialMatrix[k][`R${R}`] = 0;
+          }
+          this.initialMatrix[i][`R${R}`] = 1;
+          this.initialMatrix[i][`CxCj${i}`] = rValue;
+
+          countNewVariables += 1;
+          countTotalRows += 1;
+          ZjCjText += `"R${R}": 0,`;
+          CxCjText += `"R${R}": ${rValue},`;
+          R += 1;
           break;
-      } 
+      }
     }
-    console.log(countCXCJwFields);
-    console.log(countXbFields);
+    CxCjText += `"Xb": "Bi"}`;
+    ZjCjText += `"Z": 0}`;
+    CXCJ = JSON.parse(CxCjText);
+    ZjCj.push(JSON.parse(ZjCjText));
+    R = 1;
+    H = 1;
+    S = 1;
+    let totalCountOfCol = countNewVariables + this.variablesCount + 2;
+
+    console.log("CxCj y ZjCj respectivamente:");// !to delete
+    console.log(CXCJ);// !to delete
+    console.log(ZjCj);// !to delete
+    console.log(totalCountOfCol);// !to delete
+    
+
+    this.DoProcess(CXCJ, this.initialMatrix, ZjCj, validationsType);
+  }
+
+  DoProcess(CxCj: any, matrix: any, ZjCj: any, validationsType: any){
+    console.log("Matriz inicial");// !to delete
+    console.log(this.initialMatrix);// !to delete
+
+    let historyResults = [];
+    
     
   }
 
