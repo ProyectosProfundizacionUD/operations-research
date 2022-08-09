19b4bcae30359d9f61705f099f5e2d786c0d151b
Diego Restrepo <alejorvela@gmail.com>
2022-08-08 23:29:12 -0500

    feat: fase 1 almost complete

--- frontend/src/app/methods/simplex-two-fases/simplex-two-fases.component.ts
+++ frontend/src/app/methods/simplex-two-fases/simplex-two-fases.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concat } from 'rxjs';
 import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
@@ -66,22 +67,15 @@ export class SimplexTwoFasesComponent implements OnInit {
 
     switch (this.simplexType) {
       case 'Minimización':
-        this.simplexProcess(1);
+        this.simplexProcess("min", 1);
         break;
       case 'Maximización':
-        this.simplexProcess(-1);
+        this.simplexProcess("max", -1);
         break;
     }
   }
 
-  Minix() {
-    console.log('mini');
-  }
-  Max() {
-    console.log('maxi');
-  }
-
-  simplexProcess(rValue: number) {
+  simplexProcess(method: string, rValue: number) {
     this.initialMatrix = this.equations;
 
     let countNewVariables = 0;
@@ -168,7 +162,8 @@ export class SimplexTwoFasesComponent implements OnInit {
       countTotalRows,
       R,
       H,
-      S
+      S,
+      method
     );
   }
 
@@ -180,7 +175,8 @@ export class SimplexTwoFasesComponent implements OnInit {
     countTotalRows: number,
     R: number,
     H: number,
-    S: number
+    S: number,
+    method: string
   ) {
     //Set a correct format to historyMatrix and initialCxCj
     this.CleanMatrix(countTotalRows, CxCj, ZjCj, R, H,S);
@@ -189,42 +185,127 @@ export class SimplexTwoFasesComponent implements OnInit {
     console.log("initialCxCj");     // !to delete
     console.log(this.initialCxCj);  // !to delete
     
+    let result;
+    let startRows = 0;
+    let endRows = countTotalRows;
     
-    //aqui comienza el while
-    
-    //calcular zjcj
-    let result = this.calculateCol(
-      totalCountOfCol,
-      countTotalRows
+    do {
+      //calcular zjcj, devuelve [endFase, selectedValue, selectedIndex]
+      result = this.calculateCol(
+        startRows,
+        totalCountOfCol,
+        endRows,
+        method
       );
       
-    // *pushear matriz original y zjcj (este ultimo una vez este calculado)
-    //this.historyMatrix.push(Object.values(ZjCj));
-    console.log('History matrix'); // !to delete
-    console.log(this.historyMatrix); // !to delete
+      if(result[0] == false){
+        console.log('History matrix'); // !to delete
+        console.log(this.historyMatrix); // !to delete
 
-    // *validar si zjcj cumple con la validacion de minimizacion o maximizacion
+        for (let i = 0; i < countTotalRows + 1; i++) {
+          this.historyMatrix.push([]);          
+        }
+        
+        this.historyMatrix = this.calculateRows(
+          totalCountOfCol,
+          countTotalRows,
+          startRows,
+          endRows,
+          this.historyMatrix,
+          result[2]
+        );
 
-    // *de no ser asi, calcular la fila pivote
-    // *calcular las filas para la siguiente iteracion
-    // *terminar do while y repetir iteración
+        // *de no ser asi, calcular la fila pivote
+        // *calcular las filas para la siguiente iteracion
+        // *terminar do while y repetir iteración
 
-    // **una vez terminada la primera fase comenzamos la segunda
+        // **una vez terminada la primera fase comenzamos la segunda
+        
+        // *subimos las variables de las filas para la siguiente iteración
+        startRows += countTotalRows + 1;
+        endRows +=  countTotalRows + 1;
+      } 
+      console.log(result[0]);
+      result[0] = true; 
+          
+    } while (result[0] == false);    
+    console.log("termino");
+    
   }
 
-    calculateCol(
+  calculateRows(
     totalCountOfCol: number,
-    totalCountOfRows: number
+    totalCountOfRows: number,
+    startRows: number,
+    endRows: number,
+    historyMatrixTemp: any[],
+    selectedIndex: number
+  ){
+    console.log(`startrow: ${startRows}`);
+    console.log(`endrow: ${endRows}`);
+    console.log(`totalCountOfRows: ${totalCountOfRows}`);
+    
+    console.log("startCalculateRow");
+    let tempCandidates = []
+    let tempNumber;
+    for (let i = startRows; i < endRows; i++) {
+      if(historyMatrixTemp[i][selectedIndex] != 0){
+        tempNumber = historyMatrixTemp[i][historyMatrixTemp.length - 1] / historyMatrixTemp[i][selectedIndex];
+        tempCandidates.push(tempNumber);
+      }      
+    }
+    console.log("DivResultCandidates"); // !to delete
+    console.log(tempCandidates); // !to delete
+    
+    let indexRow = this.closestToZero(tempCandidates) + startRows;
+    console.log("indexRow " + indexRow);  // !to delete
+
+    let pivotRowNumber = historyMatrixTemp[indexRow][selectedIndex];
+    console.log(`PivotRow: ${pivotRowNumber}`); // !to delete
+    
+    //multiplicamos el inverso multiplicativo con la fila pivote
+    let InverseMultiplicative = 1/pivotRowNumber;
+    historyMatrixTemp[indexRow + totalCountOfRows + 1].push(this.initialCxCj[selectedIndex - 1])
+    for (let i = 1; i < totalCountOfCol; i++) { //!! falta pushear el valor de x!
+      historyMatrixTemp[indexRow + totalCountOfRows + 1].push(parseFloat((historyMatrixTemp[indexRow][i] * InverseMultiplicative).toFixed(2)));
+    }
+
+    //sacamos el resto de filas
+    let inverseAdit = 0;
+    tempNumber = 0
+    for (let i = startRows + totalCountOfRows + 1; i < endRows + totalCountOfRows + 1; i++) {
+      inverseAdit = historyMatrixTemp[i - totalCountOfRows - 1][selectedIndex] * - 1
+      for (let j = 1; j < totalCountOfCol; j++) {
+        if(i != (indexRow + totalCountOfRows + 1)){
+          if(j == 1)
+            historyMatrixTemp[i].push(historyMatrixTemp[i - totalCountOfRows - 1][0])  
+          
+          tempNumber = (historyMatrixTemp[indexRow + totalCountOfRows + 1][j] * inverseAdit) + historyMatrixTemp[i - totalCountOfRows - 1][j];
+          //console.log(`${historyMatrixTemp[indexRow + totalCountOfRows + 1][j]}*${inverseAdit}+${historyMatrixTemp[i - totalCountOfRows - 1][j]}=${tempNumber}`); //! to delete
+        }
+        if(i != (indexRow + totalCountOfRows + 1))
+          historyMatrixTemp[i].push(tempNumber);
+      }
+    }
+
+    console.log(historyMatrixTemp); //! to delete
+  }
+
+  calculateCol(
+    rowToStart: number,
+    totalCountOfCol: number,
+    totalCountOfRows: number,
+    method: string
   ) {
     console.log("calculateColStarted");
     this.tempArray = [0];
     let tempItem = 0;
     for (let i = 1; i < totalCountOfCol; i++) {
-      for (let j = 0; j < totalCountOfRows; j++) {
+      for (let j = rowToStart; j < totalCountOfRows; j++) {
         tempItem += (this.historyMatrix[j][0] * this.historyMatrix[j][i])
         
         if(j == (totalCountOfRows - 1))
-          if(i < totalCountOfCol - 2)
+          if(i <= totalCountOfCol - 2)
             tempItem -= this.initialCxCj[i - 1];
         
       }
@@ -233,6 +314,28 @@ export class SimplexTwoFasesComponent implements OnInit {
     }
     
     this.historyMatrix[totalCountOfRows] = this.tempArray;
+    
+    //eliminamos Z para validar entre el resto de numeros cual es el menor
+    let newArray = [0]
+    newArray = newArray.concat(this.tempArray);
+    newArray.pop()    
+
+    let selectedValue = 0;
+    let selectedIndex;
+    let endFase = false;
+    if(method === "min"){
+      selectedValue = Math.max(...newArray);
+      endFase = selectedValue <= 0 ? true : false;
+    } else {
+      selectedValue = Math.min(...newArray);
+      endFase = selectedValue >= 0 ? true : false;
+    }
+
+    console.log(selectedValue);
+    selectedIndex = this.tempArray.findIndex( (value: number) => value == selectedValue)
+    console.log(`index: ${selectedIndex}, value: ${selectedValue}, end: ${endFase}`);
+    
+    return [endFase, selectedValue, selectedIndex]
   }
 
   CleanMatrix(
@@ -290,6 +393,31 @@ export class SimplexTwoFasesComponent implements OnInit {
     this.tempArray = (Object.values(ZjCj[0]))
     this.tempArray.push(0)
     this.historyMatrix.push(this.tempArray);
+  }
+  closestToZero(numbers: number[]) {
+    if(!numbers.length){
+        return 0;
+    }
+    
+    let index = 0;
+    let closest = 0;
+    
+    for (let i = 0; i < numbers.length ; i++) {
+        if (closest === 0) {
+            closest = numbers[i];
+            index = i;
+        } else if (numbers[i] > 0 && numbers[i] <= Math.abs(closest)) {
+            closest = numbers[i];
+            index = i;
+        } else if (numbers[i] < 0 && - numbers[i] < Math.abs(closest)) {
+            closest = numbers[i];
+            index = i;
+        }
+    }
+
+    console.log("closet to zero: " + closest); // !to delete
+    
+    return index;
   }
   validations() {
     if (
